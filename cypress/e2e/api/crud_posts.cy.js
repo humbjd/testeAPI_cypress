7a171d6c1a79f23c60246fdf09b99eab835a80dd
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-12-01 14:50:47 -0300

    adicionado os testes de CRUD de posts

--- cypress/e2e/api/crud_posts.cy.js
+++ cypress/e2e/api/crud_posts.cy.js
@@ -1,19 +1,90 @@
+
 // Teste de CRUD - significado Create - Read - Update - Delete
 describe('CRUD - Posts', () => {
     
+    let postId = ''
+    let mensagem = 'Este post foi feito pelo Cypress'
+    
+
     before(() => {
         
         cy.request({
             method: 'POST',
             url: '/api/auth',
             body: {
-                email:'testeiterasys@iterasys.com',
-                password: '123456'
+                email: Cypress.env('email'),
+                password: Cypress.env('password').toString()
             }
+        }).then(() => {
+
+            Cypress.Cookies.defaults({
+                preserve: 'jwt'
+            })
         })
     })
 
-    it('teste', () => {
-        cy.log('teste')
+    it('cria um post', () => {
+
+        cy.request({
+            method: 'POST',
+            url: 'api/post',
+            body: {
+                text: mensagem
+            }
+        }).then(({ status, body }) => {
+            expect(status).to.eq(201)
+            expect(body.text).to.eq(mensagem)
+            postId = body._id
+        })
     })
+    
+    it('lê o post', () => {
+        
+        cy.request({
+            method: 'GET',
+            url: `/api/posts/${postId}`
+        }).then(({ status, body }) => {
+            expect(status).to.eq(200)
+            expect(body.text).to.eq(mensagem)
+            expect(body.likes).to.have.lengthOf(0)
+        })
+    })
+
+    it('atualiza o post', () => {
+
+        cy.request({
+            method: 'PUT',
+            url: `/api/posts/like/${postId}`
+        }).then(({ status }) => {
+            expect(status).to.eq(200)
+
+            cy.request({
+                method: 'GET',
+                url: `/api/posts/${postId}`
+            }).then(({ body }) => {
+                expect(body.likes).to.have.lengthOf(1)
+            })
+        })
+        
+    })
+
+    it('deleta o post', () => {
+        
+        cy.request({
+            method: 'DELETE',
+            url: `/api/posts/${postId}`
+        }).then(({ status }) => {
+            expect(status).to.eq(200)
+            expect(body.msg).to.eq('Post removido')
+
+            cy.request({
+                method: 'GET',
+                url: `/api/posts/${postId}`,
+                failOnStatusCode: false
+            }).then(({ status }) => {
+                expect(status).to.eq(404)
+            })
+        })
+    })
+
 })
