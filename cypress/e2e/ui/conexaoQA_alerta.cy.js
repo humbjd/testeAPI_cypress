104f5efcfbda71e22c1506ce293b03162881dd4e
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-12-07 11:25:33 -0300

    adicionado o teste de alerta

---
+++ cypress/e2e/ui/conexaoQA_alerta.cy.js
@@ -0,0 +1,32 @@
+describe('alertas', () => {
+    
+    it('valida o alerta de credencial inválida', () => {
+        
+        cy.clock()
+
+        cy.visit('/login')
+
+        // preenche o campo email
+        cy.getElement('login-email')
+            .type('hjd@teste.com')
+
+        // preenche o campo senha
+        cy.getElement('login-password')
+            .type('123456')
+        
+        // clica no botao login    
+        cy.getElement('login-submit')
+            .click()
+        
+        // valida o alerta de credencial inválida
+        cy.getElement('alert')
+            .should('have.text', 'Credenciais inválidas')
+        
+        cy.tick(10000)    
+        
+        // valida se o alerta desapareceu
+        cy.getElement('alert')
+            .should('not.exist')
+
+    })
+})
